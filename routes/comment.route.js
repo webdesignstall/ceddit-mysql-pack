348c1e720b76475d384fbf92ea6b28937f84cc1d
developeralamgirbd <developer.alamgirbd@gmail.com>
2023-11-30 19:26:40 +0600

    [Done] comment upvote and down vote

--- routes/comment.route.js
+++ routes/comment.route.js
@@ -6,7 +6,7 @@ const comment = require("../controller/comment.controller");
 router.post("/:postId", verifyToken, comment.createComment);
 router.get("/post/:id", comment.getPostComments);
 router.post("/:commentId/upvote", verifyToken, comment.upvoteComment);
-router.post("/:commentId/downvote", verifyToken, comment.downvoteComment);
+router.post("/:commentId/downvote", verifyToken, comment.upvoteComment);
 router.post("/:postId/reply/:id", verifyToken, comment.addReply);
 router.put("/:id", verifyToken, comment.updateCommentForPost);
 router.delete("/:postId/:id", verifyToken, comment.deleteCommentForPost);
